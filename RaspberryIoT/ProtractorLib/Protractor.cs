1419dbb5a20b7e18ea0ba2d35b0c46055af3c2cc
StarlightNL <David.Hijlkema@gmail.com>
2017-08-14 21:12:04 +0200

    Ported library to UWP IoT as Windows Runtime Windows Runtime means that even if it is C# it can be used with any UWP Language

---
+++ RaspberryIoT/ProtractorLib/Protractor.cs
@@ -0,0 +1,267 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.Enumeration;
+using Windows.Devices.I2c;
+using Windows.Devices.SerialCommunication;
+
+namespace ProtractorLib
+{
+    public sealed class Protractor
+    {
+        // Constants
+        const byte SERIALCOMM = 1;
+        const byte I2CCOMM = 2;
+        const byte MAXOBJECTS = 4;
+        const byte SHOWOBJ = 1;
+        const byte SHOWPATH = 2;
+        const byte LEDOFF = 3;
+        const byte MINDUR = 15;
+
+        // PROTRACTOR COMMANDS
+        const byte REQUESTDATA = 0x15;
+        const byte SCANTIME = 0x20;
+        const byte I2CADDR = 0x24;
+        const byte BAUDRATE = 0x26;
+        const byte LEDUSAGE = 0x30;
+
+        byte[] _buffer = new byte[1 + 4 * MAXOBJECTS]; // store data received from Protractor.
+        byte _numdata; // Number of data points requested from sensor during most recent read
+        byte _comm; // Tracks whether we are using I2C or Serial for communication
+
+
+        private I2cDevice protractorI2C;
+        private SerialDevice protractorSerial;
+
+        public Protractor(I2cController i2CController, int i2cAddress, bool I2CFastMode)
+        {
+            var settings = new I2cConnectionSettings(i2cAddress);
+            if(I2CFastMode) settings.BusSpeed = I2cBusSpeed.FastMode;
+            protractorI2C = i2CController.GetDevice(settings);
+            _comm = I2CCOMM;
+        }
+
+        public Protractor(string comport, int baudrate)
+        {
+            string deviceSelector = SerialDevice.GetDeviceSelector(comport);
+            var dis = DeviceInformation.FindAllAsync(deviceSelector).GetResults();
+            DeviceInformation entry = (DeviceInformation) dis[0];
+            protractorSerial = SerialDevice.FromIdAsync(entry.Id).GetResults();
+            if (protractorSerial == null) return;
+
+
+            // Configure serial settings
+            protractorSerial.WriteTimeout = TimeSpan.FromMilliseconds(1000);
+            protractorSerial.ReadTimeout = TimeSpan.FromMilliseconds(1000);
+            protractorSerial.BaudRate = (uint)baudrate;
+            protractorSerial.Parity = SerialParity.None;
+            protractorSerial.StopBits = SerialStopBitCount.One;
+            protractorSerial.DataBits = 8;
+            protractorSerial.Handshake = SerialHandshake.None;
+            
+            _comm = SERIALCOMM;
+        }
+
+        public bool Read()
+        { // get all of the data from the protractor.
+            return Read(MAXOBJECTS);
+        }
+        public bool Read(byte obs)
+        { // gets obs number of objects and obs number of paths from protractor. Returns the most visible objects and most open pathways. Minimizes data transfer for time sensitive applications.
+            if (obs > MAXOBJECTS) obs = MAXOBJECTS;
+            _numdata = obs;
+            byte numBytes = (byte) (1 + obs * 4);
+            _requestData(numBytes); // Request bytes from the obstacle sensor
+            int i = 0;
+            while (i < numBytes)
+            {
+                _buffer[i] = _read();
+                i++;
+            }
+            if (i == 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public int ObjectCount()
+        { // returns the number of objects detected
+            return (int)(_buffer[0] >> 4); // number of objects detected is the high nibble of _buffer[0]
+        }
+
+        public int PathCount()
+        { // returns the number of paths detected
+            return (int)(_buffer[0] &= 0b00001111); // number of paths detected is the low nibble of _buffer[0]
+        }
+
+        public int ObjectAngle()
+        { // returns the angle to the most visible object
+            return ObjectAngle(0);
+        }
+        public int ObjectAngle(int ob)
+        { // returns the angle to the object ob in the object list, indexed from 1. Left most object is 1. If ob exceeds number of objects detected, return zero.
+            if (ob >= ObjectCount() || ob < 0)
+            {
+                return -1;
+            }
+            else
+            {
+                int angle = map(_buffer[1 + 4 * ob], 0, 255, 0, 180);  // ob0->_buffer[1], ob1->_buffer[5], etc.
+                return angle;
+            }
+        }
+        public int ObjectVisibility()
+        { // returns the visibility of the most visible object
+            return ObjectVisibility(0);
+        }
+        public int ObjectVisibility(int ob)
+        { // returns the visibility of the object ob in the object list, indexed from 1. Left most object is 1. If ob exceeds number of objects detected, return zero.
+            if (ob >= ObjectCount() || ob < 0)
+            {
+                return -1;
+            }
+            else
+            {
+                int vis = _buffer[2 + 4 * ob];  // ob0->_buffer[2], ob1->_buffer[6], etc.
+                return vis;
+            }
+        }
+
+        public int PathAngle()
+        { // returns the angle to the most open pathway
+            return PathAngle(0);
+        }
+        public int PathAngle(int pa)
+        { // returns the angle to the path pa in the pathway list, indexed from 1. Left most path is 1. If pa exceeds number of pathways detected, return zero.
+            if (pa >= PathCount() || pa < 0)
+            {
+                return -1;
+            }
+            else
+            {
+                int angle = map(_buffer[3 + 4 * pa], 0, 255, 0, 180);  // pa0->_buffer[3], pa1->_buffer[7], etc.
+                return angle;
+            }
+        }
+        public int PathVisibility()
+        {
+            return PathVisibility(0);
+        }
+        public int PathVisibility(int pa)
+        { // returns the angle to the path pa in the pathway list, indexed from 1. Left most path is 1. If pa exceeds number of pathways detected, return zero.
+            if (pa >= PathCount() || pa < 0)
+            {
+                return -1;
+            }
+            else
+            {
+                int vis = _buffer[4 + 4 * pa];  // pa0->_buffer[4], pa1->_buffer[8], etc.
+                return vis;
+            }
+        }
+
+        /////// SETTINGS ///////
+
+        public void ScanTime(int milliSeconds)
+        {//0 = scan only when called. 1 to 15 = rescan every 15ms, >15 = rescan every time_ms milliseconds.  Default time_ms is set to 30ms.
+            if (milliSeconds >= 1 && milliSeconds <= MINDUR - 1)
+            {  // Values within 1 and 14 milliSeconds aren't allowed, the sensor requires a minimum 15 seconds to complete a scan.
+                byte[] sendData = { SCANTIME, MINDUR, 0x10 };
+                _write(sendData, 3); // Send a signal (char SCANTIME) to tell Protractor that it needs to change its time between scans to milliSeconds.
+            }
+            else if (milliSeconds >= 0 && milliSeconds <= 32767)
+            {  // Values less than 0 or greater than 32767 aren't allowed.
+                byte[] sendData = { SCANTIME, (byte)(milliSeconds & 0x00FF), (byte)(milliSeconds >> 8), 0x10 };
+                _write(sendData, 4); // Send a signal (char SCANTIME) to tell Protractor that it needs to change its time between scans to milliSeconds. 
+            }
+        }
+
+        public void SetNewI2Caddress(int newAddress)
+        { // change the I2C address. Will be stored after shutdown. See manual for instructions on restoring defaults. Default = 0x45 (69d).
+            if (newAddress >= 2 && newAddress <= 127)
+            {
+                byte[] sendData = { I2CADDR, (byte)newAddress, 0x10 };
+                _write(sendData, 3); // Send a signal (char I2CADDR) to tell Protractor that it needs to change its I2C address to the newAddress
+            }
+        }
+
+        public void SetNewSerialBaudRate(int newBaudRate)
+        { // change the Serial Bus baud rate. Will be stored after shutdown. See manual for instructions on restoring defaults. Default = 9600 baud. 0 = 1200, 1 = 2400, 2 = 4800, 3 = 9600, 4 = 19200, 5 = 28800, 6 = 38400, 7 = 57600, 8 = 115200, 9 = 230400
+            if (newBaudRate >= 1200 && newBaudRate <= 1000000)
+            {
+                byte[] sendData = { BAUDRATE, (byte)(newBaudRate & 0x00FF), (byte)(newBaudRate >> 8), (byte)(newBaudRate >> 16), 0x10 };
+                _write(sendData, 5); // Send a signal (char BAUDRATE) to tell Protractor that it needs to change its baudrate to the newBaudRate
+            }
+        }
+
+        public void LEDshowObject()
+        { // Set the feedback LEDs to follow the most visible Objects detected
+            byte[] sendData = { LEDUSAGE, SHOWOBJ, 0x10 };
+            _write(sendData, 3); // Send a signal (char LEDUSAGE) to tell Protractor that it needs to SHOWOBJ
+        }
+
+        public void LEDshowPath()
+        { // Set the feedback LEDs to follow the most open pathway detected
+            byte[] sendData = { LEDUSAGE, SHOWPATH, 0x10 };
+            _write(sendData, 3); // Send a signal (char LEDUSAGE) to tell Protractor that it needs to SHOWPATH
+        }
+
+        public void LEDoff()
+        { // Turn off the feedback LEDs
+            byte[] sendData = { LEDUSAGE, LEDOFF, 0x10 };
+            _write(sendData, 3); // Send a signal (char LEDUSAGE) to tell Protractor that it needs to turn the feedback LEDOFF
+        }
+
+        // PRIVATES //
+
+        private int map(int x, int in_min, int in_max, int out_min, int out_max)
+        {
+            return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+        }
+
+        private byte _read()
+        {
+            byte[] buff = new byte[1];
+            if (_comm == I2CCOMM)
+            {
+                protractorI2C.Read(buff);
+                return buff[0];
+            }
+            if (_comm == SERIALCOMM)
+            {
+                protractorSerial.InputStream.AsStreamForRead().Read(buff, 0, 1);
+                return buff[0];
+            }
+            return 0;
+        }
+        
+
+        private void _write(byte[] arrayBuffer, byte arrayLength)
+        {
+            if (_comm == I2CCOMM)
+            {
+                protractorI2C.Write(arrayBuffer);
+            }
+            else if (_comm == SERIALCOMM)
+            {
+                protractorSerial.OutputStream.AsStreamForWrite().Write(arrayBuffer, 0, arrayLength);
+            }
+        }
+
+        private void _requestData(byte numBytes)
+        {
+            if (_comm == SERIALCOMM)
+            {
+                byte[] sendData = { REQUESTDATA, numBytes, 0x10 };
+                _write(sendData, 3); // Send a signal (char SENDDATA) to tell Protractor that it needs to send data, tell it the number of data points to send.
+            }
+        }
+    }
+}
